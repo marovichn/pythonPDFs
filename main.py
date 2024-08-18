ab1d7f723a93186f40bb16dcd374b1b1ad0a4387
marovichn <nikolamarovic.11@gmail.com>
2024-08-18 20:39:56 +0200

    PDF Page Merger and Watermarker

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+import PyPDF2
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+inputs = sys.argv[1:]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def pdf_combiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        merger.append(pdf)
+    merger.write("super.pdf")
+    merger.close()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+pdf_combiner(inputs)
+
+# Open the merged PDF and the watermark PDF
+template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+output = PyPDF2.PdfFileWriter()
+
+# Merge the watermark with each page of the template PDF
+for i in range(template.getNumPages()):
+    page = template.getPage(i)
+    page.mergePage(watermark.getPage(0))  # Adjusted function name to lowercase merge_page
+    output.addPage(page)
+
+# Save the watermarked PDF to a new file
+with open('watermarked_output.pdf', 'wb') as file:
+    output.write(file)
